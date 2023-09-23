ca29c65af3badc4235a94800bb023af118e4bc49
Prasantaclifav <prasanta.patra@clifav.com>
2023-09-23 12:54:52 +0530

    Add Item get all delete

--- src/main/java/com/example/demo/repository/ItemRepo.java
+++ src/main/java/com/example/demo/repository/ItemRepo.java
@@ -1,11 +1,12 @@
-package com.example.demo.repository;
+package com.example.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.entity.Item;
+import com.example.demo.Entity.Item;
 @Repository
 public interface ItemRepo extends CrudRepository<Item,Long>,JpaRepository<Item,Long> {
 
+	
 }
